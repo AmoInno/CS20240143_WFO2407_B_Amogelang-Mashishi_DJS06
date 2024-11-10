ae31007e70ac7487e1ee38d3aeb925629e05841a
AmoInno <amogelangmashishi12@gmail.com>
2024-11-10 15:48:02 +0200

    first set of higher order functions

--- scripts.js
+++ scripts.js
@@ -13,3 +13,42 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+// Using forEach: Log each name
+names.forEach(name => console.log(name));
+
+// Using forEach: Log each province
+provinces.forEach(province => console.log(province));
+
+// Using forEach: Log each name with a matching province
+names.forEach((name, index) => {
+  console.log(`${name} (${provinces[index]})`)
+});
+
+// Uppercase Transformation:
+const upperCaseProvinces = provinces.map(province => province.toUpperCase());
+console.log(upperCaseProvinces);
+
+// Name Lengths:
+const nameLength = names.map(name => name.length);
+console.log(nameLength);
+
+// Alphabetically sort the provinces:
+console.log(provinces.sort());
+
+// Remove provinces containing "Cape"
+const notCape = provinces.filter(province => !province.includes("Cape"));
+console.log(notCape);
+
+// Create a boolean array to determine if a name contains the letter 'S'
+const nameContainsLetterS = names.map(name => name.toUpperCase().includes("S"));
+console.log(nameContainsLetterS);
+const letterS = names.some(name => name.toUpperCase().includes("S"));
+console.log(letterS);
+
+// Transform the names array into an object mapping names to their respective provinces.
+const objectMapping = names.reduce((accumulator, name, index) => {
+  accumulator[name] = provinces[index];
+  return accumulator
+}, {});
+console.log(objectMapping);
